30c6a5ebe638c854c91df97fbb1c0a043457139d
Islom Anvarov <islomanvarov05@gmail.com>
2026-03-04 08:44:56 +0500

    add new back base

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://ppr.tm1.uz/api",
+  baseUrl: "https://pprs-7o08.onrender.com",
   prepareHeaders: (headers) => {
     const token = localStorage.getItem("access");
     if (token) {
